db22c06f2dcd7a8c78d21a8531c77d6e6eecb7c3
Anil Patnaik <anil.patnaik@ascenderhcm.com>
2021-04-22 11:33:25 +1000

    unti tests

--- Repositories/Context/MockDatabase.cs
+++ Repositories/Context/MockDatabase.cs
@@ -8,11 +8,11 @@ namespace PetsMicroservice.Repositories.Context
     {
         public static IEnumerable<Pet> Pets => new List<Pet>
         {
-            new Pet { Id = 1, Name = "Pet1", DateOfBirth = DateTime.Now.AddYears(10), Address = "Address1" },
-            new Pet { Id = 2, Name = "Pet2", DateOfBirth = DateTime.Now, Address = "Address4" },
-            new Pet { Id = 3, Name = "Pet3", DateOfBirth = DateTime.Now.AddYears(-10), Address = "Address2" },
-            new Pet { Id = 4, Name = "Pet4", DateOfBirth = DateTime.Now.AddMonths(3), Address = "Address3" },
-            new Pet { Id = 5, Name = "Pet5", DateOfBirth = DateTime.Now.AddYears(6), Address = "Address2" },
+            new Pet { Id = 1, Name = "Pet1", DateOfBirth = new DateTime(1998, 1, 1).Date, Address = "Address1" },
+            new Pet { Id = 2, Name = "Pet2", DateOfBirth = new DateTime(2000, 10, 10).Date, Address = "Address4" },
+            new Pet { Id = 3, Name = "Pet3", DateOfBirth = new DateTime(2001, 8, 13).Date, Address = "Address2" },
+            new Pet { Id = 4, Name = "Pet4", DateOfBirth = new DateTime(1996, 4, 25).Date, Address = "Address3" },
+            new Pet { Id = 5, Name = "Pet5", DateOfBirth = new DateTime(2015, 7, 18).Date, Address = "Address2" },
         };
     }
 }
